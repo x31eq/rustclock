bb196ce453e08775bbfd88d8e9ebba8e04e115c5
Graham Breed <gbreed@gmail.com>
2020-04-18 15:47:39 +0100

    Use the 12-hour clock method

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,8 @@ fn main() {
     let qday = month % 3 + (if month == 2 || month == 11 { 1 } else { 0 });
     let week = (qday + local.day() + 5 - weekday) / 7;
     print!("{:x}{:01x}", quarter % 0x1000, week);
-    let halfday = weekday * 2 + (if local.hour() < 12 { 0 } else { 1 });
+    let (pm, hour) = local.hour12();
+    let halfday = weekday * 2 + (if pm { 1 } else { 0 });
     let ticks = (local.minute() * 4 + local.second() / 15) * 16 / 15;
-    println!(".{:01x}{:01x}{:02x}", halfday, local.hour() % 12, ticks);
+    println!(".{:01x}{:01x}{:02x}", halfday, hour, ticks);
 }
