b607cc16ad2103b3051305625f7979ce7ffbdff3
Graham Breed <gbreed@gmail.com>
2020-04-18 16:21:34 +0100

    Use flooring modulus to ensure an unsigned year
    This is a bit academic because this only works for
    times after the epoch, which means the year will be
    positive anyway.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use chrono::{Datelike, Timelike};
-use num_integer::div_mod_floor;
+use num_integer::{div_mod_floor, mod_floor};
 
 fn main() {
     let local = chrono::Local::now();
     let month = local.month0();
     let (quarter, month3) = div_mod_floor(month, 3);
-    let quarter = local.year() as u32 * 4 + quarter;
+    let quarter = mod_floor(local.year(), 1024) as u32 * 4 + quarter;
     let weekday = local.weekday().num_days_from_sunday();
     let qday = month3 + (if month == 2 || month == 11 { 1 } else { 0 });
     let week = (qday + local.day() + 5 - weekday) / 7;
-    print!("{:x}{:01x}", quarter % 0x1000, week);
+    print!("{:x}{:01x}", quarter, week);
     let (pm, hour) = local.hour12();
     let halfday = weekday * 2 + (if pm { 1 } else { 0 });
     let ticks = (local.minute() * 4 + local.second() / 15) * 16 / 15;
