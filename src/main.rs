89bcd67ddc169066fd3cf714025beb4ec1b55dd3
Graham Breed <gbreed@gmail.com>
2020-04-18 15:44:56 +0100

    festamp in Rust

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+extern crate chrono;
+
+use chrono::{Datelike, Timelike};
+
+fn main() {
+    let local = chrono::Local::now();
+    let month = local.month0();
+    let quarter = local.year() as u32 * 4 + month / 3;
+    let weekday = local.weekday().num_days_from_sunday();
+    let qday = month % 3 + (if month == 2 || month == 11 { 1 } else { 0 });
+    let week = (qday + local.day() + 5 - weekday) / 7;
+    print!("{:x}{:01x}", quarter % 0x1000, week);
+    let halfday = weekday * 2 + (if local.hour() < 12 { 0 } else { 1 });
+    let ticks = (local.minute() * 4 + local.second() / 15) * 16 / 15;
+    println!(".{:01x}{:01x}{:02x}", halfday, local.hour() % 12, ticks);
+}
