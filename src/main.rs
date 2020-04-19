4c5491e1386b237b407d277cd10454d1f4ba267f
Graham Breed <gbreed@gmail.com>
2020-04-19 17:31:29 +0100

    struct Time

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 use chrono::{Local, Datelike, Timelike};
 use num_integer::{div_mod_floor, mod_floor};
 
+struct Time {
+    quarter: i32,
+    week: u8,
+    halfday: u8,
+    hour: u8,
+    ticks: u8,
+}
+
 fn main() {
     let local = Local::now();
     let month = local.month0();
     let (quarter, month3) = div_mod_floor(month, 3);
-    let quarter = mod_floor(local.year(), 1024) as u32 * 4 + quarter;
     let weekday = local.weekday().num_days_from_sunday();
     let qday = month3 + (month == 2 || month == 11) as u32;
-    let week = (qday + local.day() + 5 - weekday) / 7;
-    print!("{:03x}{:01x}", quarter, week);
     let (pm, hour) = local.hour12();
-    let halfday = weekday * 2 + pm as u32;
-    let ticks = (local.minute() * 4 + local.second() / 15) * 16 / 15;
-    println!(".{:01x}{:01x}{:02x}", halfday, hour % 12, ticks);
+    let now = Time {
+        quarter: local.year() * 4 + quarter as i32,
+        week: ((qday + local.day() + 5 - weekday) / 7) as u8,
+        halfday: weekday as u8 * 2 + pm as u8,
+        hour: hour as u8 % 12,
+        ticks: ((local.minute() * 4 + local.second() / 15) * 16 / 15) as u8,
+    };
+    print!("{:03x}{:01x}", mod_floor(now.quarter, 0x1000), now.week);
+    println!(".{:01x}{:01x}{:02x}", now.halfday, now.hour, now.ticks);
 }
