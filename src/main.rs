f8bda9d0f1cca7d26cd9550d59c4513dd3f042ed
Graham Breed <gbreed@gmail.com>
2020-04-19 17:34:07 +0100

    Current Time initializer

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,26 @@ struct Time {
     ticks: u8,
 }
 
+impl Time {
+    fn now() -> Self {
+        let local = Local::now();
+        let month = local.month0();
+        let (quarter, month3) = div_mod_floor(month, 3);
+        let weekday = local.weekday().num_days_from_sunday();
+        let qday = month3 + (month == 2 || month == 11) as u32;
+        let (pm, hour) = local.hour12();
+        Time {
+            quarter: local.year() * 4 + quarter as i32,
+            week: ((qday + local.day() + 5 - weekday) / 7) as u8,
+            halfday: weekday as u8 * 2 + pm as u8,
+            hour: hour as u8 % 12,
+            ticks: ((local.minute() * 4 + local.second() / 15) * 16 / 15) as u8,
+        }
+    }
+}
+
 fn main() {
-    let local = Local::now();
-    let month = local.month0();
-    let (quarter, month3) = div_mod_floor(month, 3);
-    let weekday = local.weekday().num_days_from_sunday();
-    let qday = month3 + (month == 2 || month == 11) as u32;
-    let (pm, hour) = local.hour12();
-    let now = Time {
-        quarter: local.year() * 4 + quarter as i32,
-        week: ((qday + local.day() + 5 - weekday) / 7) as u8,
-        halfday: weekday as u8 * 2 + pm as u8,
-        hour: hour as u8 % 12,
-        ticks: ((local.minute() * 4 + local.second() / 15) * 16 / 15) as u8,
-    };
+    let now = Time::now();
     print!("{:03x}{:01x}", mod_floor(now.quarter, 0x1000), now.week);
     println!(".{:01x}{:01x}{:02x}", now.halfday, now.hour, now.ticks);
 }
