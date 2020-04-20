010dbb64484a6911b1306fae28e7d39516f6d769
Graham Breed <gbreed@gmail.com>
2020-04-20 16:55:18 +0100

    Leap second support

--- src/lib.rs
+++ src/lib.rs
@@ -20,14 +20,15 @@ impl Time {
         let weekday = local.tm_wday;
         let qday = month3 * 38 - (month == 2 || month == 11) as i32;
         let (pm, hour) = div_mod_floor(local.tm_hour, 12);
-        let (tick, sec) = div_mod_floor(local.tm_sec, 15);
+        let leap_second = local.tm_sec / 60;
+        let (tick, sec) = div_mod_floor(local.tm_sec - leap_second, 15);
         Time {
             quarter: (local.tm_year + 1900) * 4 + quarter as i32,
             week: ((qday + local.tm_mday + 5 - weekday) / 7) as u8,
             halfday: weekday as u8 * 2 + pm as u8,
             hour: hour as u8,
             tick: ((local.tm_min * 4 + tick) * 16 / 15) as u8,
-            sec: sec as u8,
+            sec: (sec + leap_second) as u8,
         }
     }
 }
