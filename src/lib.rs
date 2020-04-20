c341bd132e14d96647d467b052e7d4ef31fb1681
Graham Breed <gbreed@gmail.com>
2020-04-20 17:54:41 +0100

    feedate functionality but output tacked on to the end of feestamp for now

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
+use num_integer::{div_floor, div_mod_floor, mod_floor};
 /// Current time in bfee format (no leap seconds)
-
 use time;
-use num_integer::div_mod_floor;
 
 pub struct Time {
     pub quarter: i32,
@@ -31,4 +30,44 @@ impl Time {
             sec: (sec + leap_second) as u8,
         }
     }
+
+    pub fn decode(self: Time) -> time::Tm {
+        let (year, quarter) = div_mod_floor(self.quarter, 4);
+        let year = 1920 + mod_floor(year + 128, 1024);
+        let month =
+            quarter * 3 + (self.week * 16 + self.halfday) as i32 / 0x55;
+        let k = (month % 3) * 38 + 5 - (month == 2 || month == 11) as i32;
+        let day = (self.week * 7 + self.halfday / 2) as i32 - k
+            + (1 + k - month_weekday(year, month)) % 7;
+        let toc = self.tick / 16 * 15 + self.tick % 16;
+        time::Tm {
+            tm_year: year - 1900,
+            tm_mon: month,
+            tm_mday: day as i32,
+            tm_wday: (self.halfday / 2) as i32,
+            tm_hour: (self.hour + 12 * (self.halfday & 1)) as i32,
+            tm_min: toc as i32 / 4,
+            tm_sec: (toc % 4) as i32 * 15 + self.sec as i32,
+            tm_nsec: 0,
+            tm_yday: -1,   // not known
+            tm_isdst: -1,  // not known
+            tm_utcoff: -1, // not known
+        }
+    }
+}
+
+/// Weekday (Sunday is 0) of the first day of the month
+/// month is 0 for January
+fn month_weekday(year: i32, month: i32) -> i32 {
+    // Based on RFC 3339 Appendix B
+    let mut y = year;
+    let mut m = month - 1;
+    if m < 1 {
+        m += 12;
+        y -= 1;
+    }
+    let (cent, y) = div_mod_floor(y, 100);
+    let day =
+        (26 * m - 2) / 10 + 1 + y + y / 4 + div_floor(cent, 4) + 5 * cent;
+    mod_floor(day, 7)
 }
