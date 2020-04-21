d6b1c137eb4f00ff011ec2985cd0b2dea3e47ddf
Graham Breed <gbreed@gmail.com>
2020-04-21 17:20:49 +0100

    Read times differently

--- src/bin/festamp.rs
+++ src/bin/festamp.rs
@@ -4,29 +4,26 @@ use std::env;
 use time;
 
 fn main() {
-    let mut args = env::args().skip(1);
-    let instant = if let Some(date_part) = args.next() {
-        Time::from_tm(if let Some(time_part) = args.next() {
-            time::strptime(
-                &format!("{} {}", date_part, time_part),
-                "%Y-%m-%d %H:%M:%S",
-            )
-            .expect("Bad time format")
-        } else if date_part.find('-') == None {
-            // date_part is really a time!
-            time::strptime(
-                &format!("1984-01-01 {}", date_part),
-                "%Y-%m-%d %H:%M:%S",
-            )
-            .expect("Bad time format")
-        } else if date_part.find(' ') == None {
-            time::strptime(&date_part, "%Y-%m-%d").expect("Bad date format")
-        } else {
-            time::strptime(&date_part, "%Y-%m-%d %H:%M:%S")
-                .expect("Bad date format")
-        })
-    } else {
+    let args = env::args().skip(1);
+    let instant = if args.len() == 0 {
         Time::now()
+    } else {
+        Time::from_tm({
+            let datetime = args.collect::<Vec<String>>().join(" ");
+            if datetime.find('-') == None {
+                time::strptime(
+                    &format!("1984-01-01 {}", datetime),
+                    "%Y-%m-%d %H:%M:%S",
+                )
+                .expect("Bad time format")
+            } else if datetime.find(' ') == None {
+                time::strptime(&datetime, "%Y-%m-%d")
+                    .expect("Bad date format")
+            } else {
+                time::strptime(&datetime, "%Y-%m-%d %H:%M:%S")
+                    .expect("Bad date format")
+            }
+        })
     };
     print!(
         "{:03x}{:x}",
